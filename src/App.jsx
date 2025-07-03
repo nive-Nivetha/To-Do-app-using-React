4b4f91bbf2c60a076694f9e82fc17bf9e9eba342
Nivetha <nivenivetha0714@gmai.com>
2025-07-03 11:27:02 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Todo from "./Todo"
+
+function App() {
+  
+
+  return (
+    <>
+      <h2>TO-DO APP</h2>
+      <Todo/>
+    </>
+  )
+}
+
+export default App
---
+++ src/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo() {
+  return (
+    <div>
+        <form>
+            <input type='text'/>
+            <button>Add Task</button>
+        </form>
+    </div>
+  )
+}
+
+export default Todo
